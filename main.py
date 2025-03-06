e9142ab03446398c96f83af9bc8d9f791af4a3a1
irami23 <iranramirez.acg@gmail.com>
2025-03-06 02:11:51 -0600

    added sort_on functionand sorted list of dictionaries function to stats.py. Also, printed report on main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_char
+import stats
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,8 +7,20 @@ def get_book_text(filepath):
 
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(book_contents)
-    print(f'{num_words} words found in the document')
-    print(get_num_char(book_contents))
+    num_words = stats.get_num_words(book_contents)
+    #print(f'{num_words} words found in the document')
+    #print(stats.get_num_char(book_contents))
+    char_cnt = stats.sorted_list(stats.get_num_char(book_contents))
+    char_cnt.sort(reverse=True, key=stats.sort_on)
+    #print(char_cnt)
+    print(
+    f'''============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------''')
+    for i in char_cnt:
+        print(f'{i["char"]}: {i["cnt"]}')
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def get_num_char(bookString):
         else:
             num_char[str(char.lower())] = 1
     return num_char
+
+def sorted_list(dict):
+    char_count = []
+    for i in dict:
+        if i.isalpha():
+            tmp = {"char": i, "cnt": dict[i]}
+            char_count.append(tmp)
+        else:
+            continue
+    return char_count
+
+def sort_on(dict):
+    return dict["cnt"]
