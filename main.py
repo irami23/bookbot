a2ba9cba5a9c0624b8343fa57dd5fd69a2470efb
irami23 <iranramirez.acg@gmail.com>
2025-03-06 18:45:20 -0600

    added ability to add arguments when running main.py so you can test against any book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import stats
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,21 +7,25 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
-    num_words = stats.get_num_words(book_contents)
-    #print(f'{num_words} words found in the document')
-    #print(stats.get_num_char(book_contents))
-    char_cnt = stats.sorted_list(stats.get_num_char(book_contents))
-    char_cnt.sort(reverse=True, key=stats.sort_on)
-    #print(char_cnt)
-    print(
-    f'''============ BOOKBOT ============
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    else:
+        book_contents = get_book_text(sys.argv[1])
+        num_words = stats.get_num_words(book_contents)
+        #print(f'{num_words} words found in the document')
+        #print(stats.get_num_char(book_contents))
+        char_cnt = stats.sorted_list(stats.get_num_char(book_contents))
+        char_cnt.sort(reverse=True, key=stats.sort_on)
+        #print(char_cnt)
+        print(
+        f'''============ BOOKBOT ============
 Analyzing book found at books/frankenstein.txt
 ----------- Word Count ----------
 Found {num_words} total words
 --------- Character Count -------''')
-    for i in char_cnt:
-        print(f'{i["char"]}: {i["cnt"]}')
-    print("============= END ===============")
+        for i in char_cnt:
+            print(f'{i["char"]}: {i["cnt"]}')
+        print("============= END ===============")
 
 main()
